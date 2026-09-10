beb7a88c53c090e4a6c19bba6c63ffe600678734
Saarvi <saarvisac35@gmail.com>
2026-09-10 17:27:34 +0530

    fix: add Kubernetes health endpoint

--- beleva-website/ecommerce/urls.py
+++ beleva-website/ecommerce/urls.py
@@ -15,13 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path , include
 from django.conf.urls.static import static
 from django.conf import settings
 from accounts import views
 
 
+def healthz(request):
+    """Lightweight Kubernetes health endpoint that does not render a page."""
+    return HttpResponse("ok", content_type="text/plain")
+
+
 urlpatterns = [
+    path('healthz/', healthz, name='healthz'),
     path('admin/', admin.site.urls),
 
     path('',include('products.urls')) , 
